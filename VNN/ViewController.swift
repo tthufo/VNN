92918b3569304531aafdad674a736945b34fcb86
Andrew <Andrew.tran@innovatube.com>
2018-09-21 00:30:32 +0700

    ahiho

---
+++ VNN/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VNN
+//
+//  Created by Thanh Hai Tran on 9/21/18.
+//  Copyright © 2018 Thanh Hai Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
